9d502ec2cfb01d14e6257dee9d206b4d03f657c2
Redha Boubakour <redha.boubakour@yahoo.fr>
2023-09-18 09:52:16 +0100

    commit controlled elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,100 @@
-import "./App.css";
+import { useState } from "react";
 
-function App() {
-    return <p>Hello</p>;
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+    { id: 3, description: "Charger", quantity: 12, packed: false },
+];
+
+export default function App() {
+    return (
+        <div className="app">
+            <Logo />
+            <Form />
+            <PackingList />
+            <Stats />
+        </div>
+    );
 }
 
-export default App;
+function Logo() {
+    return <h1>🌴 Far Away </h1>;
+}
+
+function Form() {
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+
+    function handlerSubmit(e) {
+        e.preventDefault();
+
+        if (!description) return;
+
+        const newItem = {
+            description,
+            quantity,
+            packed: false,
+            id: Date.now(),
+        };
+
+        setDescription("");
+        setQuantity(1);
+        console.log(newItem);
+    }
+
+    return (
+        <form className="add-form" onSubmit={handlerSubmit}>
+            <h3>What do you need for your trip ?</h3>
+            <select
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+            >
+                {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+                    <option value={num} key={num}>
+                        {num}
+                    </option>
+                ))}
+            </select>
+            <input
+                type="text"
+                placeholder="Item..."
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            ></input>
+            <button>Add</button>
+        </form>
+    );
+}
+
+function PackingList() {
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} key={item.id} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+function Item({ item }) {
+    return (
+        <li>
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+                {item.quantity} -{item.description}
+            </span>
+            <button>❌</button>
+        </li>
+    );
+}
+
+function Stats() {
+    return (
+        <footer className="stats">
+            <p>
+                You have X item(s) on your list, and you already packed X (X%)
+            </p>
+        </footer>
+    );
+}
